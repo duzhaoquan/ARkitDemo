5dfe762c64ca6ed6b1a45d94c32788582c8a4c90
duzhaoquan <zhaoquan.du@mxplayer.in>
2024-01-09 19:58:33 +0800

    Initial Commit

---
+++ ARKitDeamo/ARKitDeamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitDeamoApp.swift
+//  ARKitDeamo
+//
+//  Created by zhaoquan du on 2024/1/9.
+//
+
+import SwiftUI
+
+@main
+struct ARKitDeamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARKitDeamo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARKitDeamo
+//
+//  Created by zhaoquan du on 2024/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
